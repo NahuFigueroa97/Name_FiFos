1222f98cceb72c0af19ec3313804eecd122ebfb2
Nahuel Figueroa <nahuelfigueroa978@hotmail.com>
2021-08-19 17:17:35 -0300

    final

--- ColasNombradas_Writer/writer.c
+++ ColasNombradas_Writer/writer.c
@@ -1,116 +1,97 @@
-#include <stdlib.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <errno.h>
-#include <signal.h>
+#include <string.h>
 #include <fcntl.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <signal.h>
 
-#define Name "MiFifo"
-#define buffer1 "SIGN: 1"
-#define buffer2 "SIGN: 2"
-#define SizeBuffer 20
-#define Message "XXXX: "
-
+#define FIFO_NAME "myfifo"
+#define BUFFER_SIZE 300
 volatile sig_atomic_t signalNumber;
+void handler1(int sig);
+void handler2(int sig);
 
-void handler1(int sig){
-
-    signalNumber = 1;
-
-}
-void handler2(int sig){
-
-    signalNumber = 2;
-
-}
-
-int main(){
-
-char bufferOut[SizeBuffer];
-int32_t code = 0, fd = 0;
-uint32_t bytesWrite = 0;
-struct sigaction sa;
-sa.sa_handler = handler1;
-sa.sa_flags = 0;
-
-if (sigemptyset(&sa.sa_mask) == -1){
-
-    perror("sigemptyset");
-    exit(1);
-
-}
-if (sigaction(SIGUSR1, &sa, NULL) == -1){
-
-    perror("sigaction");
-    exit(1);
-
-}
-sa.sa_handler = handler2;
-sa.sa_flags = 0;
-
-if (sigemptyset(&sa.sa_mask) == -1){
-
-    perror("sigemptyset");
-    exit(1);
-
-}
-
-if (sigaction(SIGUSR2, &sa, NULL) == -1){
-
-    perror("sigaction");
-    exit(1);
-
-}
-
-if ( (code = mknod(Name, S_IFIFO | 0644, 0)) < -1) {
-
-printf(" Error creando la cola nombrada: %d\n", code);
-exit(1);
-
-}
-
-printf("Esperando por un proceso lector \n");
-
-if ( (fd = open(Name, O_WRONLY)) < 0 ){
-
-printf(" Error abriendo la cola nombrada: %d\n", fd);
-exit(1);
-
-}
-
-printf("Tengo un proceso lector!! \n");
-
-while (1){
-
-    fgets(bufferOut, SizeBuffer, stdin);
-
-    char bufferIn[sizeof(Message) + SizeBuffer];
-
-    if (signalNumber != 0){
-
-        sprintf(bufferIn, "SIGN:%d\n", signalNumber);
-        signalNumber = 0;
-
-    }
-    else
+int main(void)
+{
+    char outputBuffer[BUFFER_SIZE];
+    uint32_t bytesWrote;
+    int32_t returnCode, fd;
+    struct sigaction sa;
+    sa.sa_handler = handler1;
+    sa.sa_flags = 0;
+    if (sigemptyset(&sa.sa_mask) == -1)
     {
-        sprintf(bufferIn, "DATA:%s", bufferOut);
+        perror("sigemptyset");
+        exit(1);
     }
-
-    if( (bytesWrite = write(fd, bufferIn, strlen(bufferIn)-1) == -1) ){
-
-        perror("Write: ");
-
-    }else {
-        
-        printf("Escritor: se escribieron %d bytes\n", bytesWrite);
-
+    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+        exit(1);
     }
-}
-
+    sa.sa_handler = handler2;
+    sa.sa_flags = 0;
+    if (sigemptyset(&sa.sa_mask) == -1)
+    {
+        perror("sigemptyset");
+        exit(1);
+    }
+    if (sigaction(SIGUSR2, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+        exit(1);
+    }
+    /* Create named fifo. -1 means already exists so no action if already exists */
+    if ((returnCode = mknod(FIFO_NAME, S_IFIFO | 0666, 0)) < -1)
+    {
+        printf("Error creating named fifo: %d\n", returnCode);
+        exit(1);
+    }
+    /* Open named fifo. Blocks until other process opens it */
+    printf("waiting for readers...\n");
+    if ((fd = open(FIFO_NAME, O_WRONLY)) < 0)
+    {
+        printf("Error opening named fifo file: %d\n", fd);
+        exit(1);
+    }
+    /* open syscalls returned without error -> other process attached to named fifo */
+    printf("got a reader--type some stuff\n");
+    /* Loop forever */
+    while (1)
+    {
+        /* Get some text from console */
+        fgets(outputBuffer, BUFFER_SIZE, stdin);
+        char dataBuffer[sizeof("XXXX:") + BUFFER_SIZE];
+        if (signalNumber != 0)
+        {
+            sprintf(dataBuffer, "SIGN:%d\n", signalNumber);
+            signalNumber = 0;
+        }
+        else
+        {
+            sprintf(dataBuffer, "DATA:%s", outputBuffer);
+        }
+        /* Write buffer to named fifo. Strlen - 1 to avoid sending \n char */
+        if ((bytesWrote = write(fd, dataBuffer, strlen(dataBuffer) - 1)) == -1)
+        {
+            perror("write");
+        }
+        else
+        {
+            printf("writer: wrote %d bytes\n", bytesWrote);
+        }
+    }
     return 0;
+}
+void handler1(int sig)
+{
+    signalNumber = 1;
+}
+void handler2(int sig)
+{
+    signalNumber = 2;
 }
